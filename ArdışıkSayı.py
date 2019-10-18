77ec4474e4f9d2baf1e3173d1738e3fbffcbd76c
selocan1563 <sselcukiseri@hotmail.com>
2019-10-18 11:48:48 +0300

    Update ArdışıkSayı.py

--- ArdışıkSayı.py
+++ ArdışıkSayı.py
@@ -1,12 +1,12 @@
 # Her Türlü Ardışık Dizinin Terimleri Toplamını Veren Program
 
-a = input("İlk Terim:")
-b = input("Son Terim:")
-c = input("Ardışık İki Terimin Farkı:")
+a = input("İlk Terim:") ##ilk terimi alır
+b = input("Son Terim:") ##ikinci terimi alır
+c = input("Ardışık İki Terimin Farkı:") ##farklarını alır
 
 sonuç = (((int(a) + int(b)) * (int(b) - int(a) + int(c))) / (2 * int(c)))
 
-print("Sonuç:", int(sonuç))
+print("Sonuç:", int(sonuç)) #sonucu ekrana çıktı verir
 
 # Her Türlü Ardışık Dizinin Terimleri Toplamını Veren Program
 
